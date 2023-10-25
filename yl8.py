7a2254adb21b67813c60aa8570db25e05aa39725
araamat <raamatandrus@gmail.com>
2023-10-25 11:48:27 +0300

    yl9 wip

--- yl8.py
+++ yl8.py
@@ -1,2 +1,11 @@
 # Kirjutada programm, mis kontrollib, kas antud positiivne täisarv on liig- või lihtaasta number.
 # Aasta on liigaasta kui ta jagub neljasajaga või jagub neljaga ja ei jagu sajaga.
+
+# Kasutaja sisestab aasta numbri.
+
+
+year= int(input("Sisesta aasta: "))
+if year % 400 == 0 or year % 4 == 0 and year % 100 != 0:
+    print(str(year) + " on liigaasta")
+else:
+    print(year, "on lihtaasta")
