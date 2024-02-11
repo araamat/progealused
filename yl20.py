b78728bec321f4b86e0ca3f8b788f8e6d02ce0ed
araamat <raamatandrus@gmail.com>
2024-02-11 20:54:15 +0200

    kordamine

--- yl20.py
+++ yl20.py
@@ -14,3 +14,10 @@ b = int(b)
 
 for a in range(0, 13):
     print(b, "x", a, "=", a * b)
+
+# Muutuja b algväärtuseks määratakse tühi string "".
+# Seejärel käivitatakse while tsükkel, mis töötab seni, kuni kasutaja sisestatud string ei ole arvuline (kasutades isnumeric() meetodit).
+# Kui kasutaja sisend ei ole arvuline, küsitakse kasutajalt uut sisendit, mida kontrollitakse uuesti.
+# Kui sisend on arvuline, muudetakse see täisarvuks int() funktsiooniga.
+# Seejärel käivitatakse for tsükkel vahemikus 0 kuni 12 (sealhulgas 0 ja 12), mille sees korrutatakse kasutaja 
+    #sisestatud arv iga arvuga vahemikus 0 kuni 12 ning tulemus väljastatakse.
--- yl21.py
+++ yl21.py
@@ -23,3 +23,13 @@ while kasutaja_pakkumine != salajane_arv:
         print("Pakkumine on liiga suur. Proovi uuesti.")
     else:
         print(f"Õige! Arvuti valitud arv oli {salajane_arv}. Sa pakkusid õigesti {katsete_arv} katsega.")
+
+# salajane_arv muutuja määratakse juhuslikule arvule vahemikus 0 kuni 100, kasutades random.randint() funktsiooni.
+# kasutaja_pakkumine muutuja algväärtus on None, mis tähendab, et kasutajalt pole veel sisendit saadud.
+# katsete_arv muutuja hoiab meeles, mitu korda kasutaja on pakkunud.
+# while tsükkel töötab seni, kuni kasutaja pakkumine ei ole võrdne salajase arvuga.
+# Iga tsükli iteratsiooni käigus küsitakse kasutajalt pakkumist ning kontrollitakse, kas see on suurem või väiksem kui salajane arv.
+# Kui kasutaja pakkumine on väiksem, väljastatakse vastav teade.
+# Kui kasutaja pakkumine on suurem, väljastatakse vastav teade.
+# Kui kasutaja pakkumine on õige, väljastatakse vastav teade ning mäng lõpetatakse.
+# katsete_arv hoiab meeles, mitu korda kasutaja on pakkunud, ning see väljastatakse lõppseisundis, kui kasutaja on arvu ära arvanud.
